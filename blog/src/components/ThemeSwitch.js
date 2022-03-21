34492d596796f5872a37a05719a1c94db5c05ff7
bogdanmogos <bogdan.mogos@vodafone.com>
2022-03-21 16:40:06 +0200

    refactoring

--- blog/src/components/ThemeSwitch.js
+++ blog/src/components/ThemeSwitch.js
@@ -5,29 +5,20 @@ export default function ThemeSwitch() {
   return (
     <ThemeContext.Consumer>
       {({ theme, toggleTheme, currentTheme }) => (
-        <>
-          {theme === "dark" && currentTheme === "dark" ? (
-            <div className="theme-switch-box">
-              <button
-                type="button"
-                className="theme-switch"
-                onClick={toggleTheme}
-              >
-                <div className="fas fa-sun round"></div>
-              </button>
+        <div className="theme-switch-box">
+          <button
+            type="button"
+            className="theme-switch"
+            onClick={toggleTheme}
+          >
+            <div className={theme === 'dark' && currentTheme === 'dark'
+              ? "fas fa-sun round"
+              : "far fa-moon round"}>
+
             </div>
-          ) : (
-            <div className="theme-switch-box">
-              <button
-                type="button"
-                className="theme-switch"
-                onClick={toggleTheme}
-              >
-                <div className="far fa-moon round"></div>
-              </button>
-            </div>
-          )}
-        </>
+          </button>
+        </div>
+
       )}
     </ThemeContext.Consumer>
   );
